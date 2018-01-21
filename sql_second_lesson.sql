7859c128564047ed179c88741d233f077886243d
juszczakmarek <32249612+juszczakmarek@users.noreply.github.com>
2018-01-21 12:16:03 +0100

    Add files via upload

--- sql_second_lesson.sql
+++ sql_second_lesson.sql
@@ -7,3 +7,10 @@ inner join dept_emp de
 	on e.emp_no = de.emp_no
 inner join departments d
 	on de.dept_no = d.dept_no;
+    
+select e.emp_no, e.first_name, e.last_name, dm.from_date, dm.to_date, d.dept_no, d.dept_name
+from employees e
+inner join dept_manager dm
+	on e.emp_no = dm.emp_no
+inner join departments d
+	on dm.dept_no = d.dept_no;
