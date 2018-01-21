1831ca4e6ec2d7a95279ca696a1fbdab4564ebfb
juszczakmarek <32249612+juszczakmarek@users.noreply.github.com>
2018-01-21 11:38:54 +0100

    Add files via upload

--- sql_second_lesson.sql
+++ sql_second_lesson.sql
@@ -1,1 +1,9 @@
 -- using sample employees db
+use employees;
+
+select e.emp_no, e.first_name, e.last_name, d.dept_no, d.dept_name
+from employees e
+inner join dept_emp de
+	on e.emp_no = de.emp_no
+inner join departments d
+	on de.dept_no = d.dept_no;
